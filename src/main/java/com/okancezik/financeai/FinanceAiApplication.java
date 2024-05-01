c99967013e63d6331443a4cb3f7c048fd307d65f
Okan Cezik <okacezik@gmail.com>
2024-05-01 17:08:25 +0300

    init

---
+++ src/main/java/com/okancezik/financeai/FinanceAiApplication.java
@@ -0,0 +1,13 @@
+package com.okancezik.financeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/okancezik/financeai/FinanceAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okancezik.financeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
